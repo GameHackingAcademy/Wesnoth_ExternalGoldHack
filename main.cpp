f585000ab88cf88484be0f51bb1547fbf10c416d
attilathedud <attilathedud@gmail.com>
2020-07-05 11:52:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,28 +28,29 @@ int main(int argc, char** argv) {
 
     // Get the process id for the Wesnoth process. GetWindowThreadProcessId does not return a process id, but
     // rather fills a provided variable with its value, hence the &.
-	DWORD process_id = 0;
-	GetWindowThreadProcessId(wesnoth_window, &process_id);
+    DWORD process_id = 0;
+    GetWindowThreadProcessId(wesnoth_window, &process_id);
 
     // Open our Wesnoth process. PROCESS_ALL_ACCESS means we can both read and write to the process. However,
     // it also means that this program must be executed as an administrator to work.
-	HANDLE wesnoth_process = OpenProcess(PROCESS_ALL_ACCESS, true, process_id);
+    HANDLE wesnoth_process = OpenProcess(PROCESS_ALL_ACCESS, true, process_id);
 
     // Read the value at 0x017EED18 and place its value into the variable gold_value. 
-	DWORD gold_value = 0;
-	DWORD bytes_read = 0;
-	ReadProcessMemory(wesnoth_process, (void*)0x017EED18, &gold_value, 4, &bytes_read);
+    DWORD gold_value = 0;
+    DWORD bytes_read = 0;
+    ReadProcessMemory(wesnoth_process, (void*)0x017EED18, &gold_value, 4, &bytes_read);
 
     // Add 0xA90 to the value read from the last step and then read the value at that new address. These 
     // offsets are covered in https://gamehacking.academy/lesson/13
-	gold_value += 0xA90;
-	ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
+    gold_value += 0xA90;
+    ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
 
     // Add 4 to the gold_value, which will then be pointing at the player's current gold address.
     // Write the value of new_gold_value (555) into this address
-	gold_value += 4;
-	DWORD new_gold_value = 555;
-	DWORD bytes_written = 0;
-	WriteProcessMemory(wesnoth_process, (void*)gold_value, &new_gold_value, 4, &bytes_written);
-	return 0;
+    gold_value += 4;
+    DWORD new_gold_value = 555;
+    DWORD bytes_written = 0;
+    WriteProcessMemory(wesnoth_process, (void*)gold_value, &new_gold_value, 4, &bytes_written);
+	
+    return 0;
 }
